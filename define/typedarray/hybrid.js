58a97ed64342def5d7bdc716308e7bf5f3cc83e9
Willt125 <watrammell@gmail.com>
2017-05-17 14:18:33 -0400

    Update hybrid.js
    Translated and converted to dot notation

--- define/typedarray/hybrid.js
+++ define/typedarray/hybrid.js
@@ -4,12 +4,12 @@
 
 goog.provide('USE_TYPEDARRAY');
 
-// Safari が typeof Uint8Array === 'object' になるため、
-// 未定義か否かで Typed Array の使用を決定する
+// Since Safari becomes typeof Uint8Array === 'object', it decides to use
+// the typed array depending on whether it is undefined or not
 
 /** @const {number} use typed array flag. */
 var USE_TYPEDARRAY = (
-  goog.global['Uint8Array'] !== void 0 &&
-  goog.global['Uint16Array'] !== void 0 &&
-  goog.global['Uint32Array'] !== void 0
+  goog.global.Uint8Array !== void 0 &&
+  goog.global.Uint16Array !== void 0 &&
+  goog.global.Uint32Array !== void 0
 );
